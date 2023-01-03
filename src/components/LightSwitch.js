cfbbbba132fa789996a9b3c6e9563da2c4fd79e3
monicaarganda <argandamonica1@gmail.com>
2023-01-02 18:10:41 -0800

    completed 3-5 user stories

--- src/components/LightSwitch.js
+++ src/components/LightSwitch.js
@@ -1,13 +1,22 @@
 import React from "react"
 
-const LightSwitch = () => {
+const LightSwitch = (props) => {
+  console.log('prop:', props)
+
   return (
     <>
-      <div className="lightbulb" >
-        <h2>off</h2>
+      <div 
+        className="lightbulb" 
+        onClick={props.flipTheSwitch} 
+        style={{backgroundColor: props.color}}
+        >
+      <h2>{props.lightSwitchStatus}</h2>
       </div>
     </>
   )
 }
 
 export default LightSwitch
+
+// style={{backgroundColor: props.lightSwitchStatus === "off" ? 'white' : 'yellow'}}
+// ? 'background-white' : 'background-yellow'}
